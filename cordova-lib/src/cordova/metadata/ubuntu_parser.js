b9a16fea53f4b24d0df7c75f272dd0529c845917
Jean-Francois Moy <jeanfrancois.moy@gmail.com>
2014-10-24 12:08:06 -0400

    Improved the update_from_config function, using the current config.xml and no longer storing a reference to the config.

--- cordova-lib/src/cordova/metadata/ubuntu_parser.js
+++ cordova-lib/src/cordova/metadata/ubuntu_parser.js
@@ -32,8 +32,9 @@ var fs            = require('fs'),
 
 module.exports = function(project) {
     this.path = project;
-    this.config = new ConfigParser(this.config_xml());
-    this.update_manifest();
+    this.manifest = path.join(this.path, 'manifest.json');
+    this.desktop = path.join(this.path, 'cordova.desktop');
+    this.apparmor = path.join(this.path, 'apparmor.json');
 };
 
 function sanitize(str) {
@@ -42,106 +43,140 @@ function sanitize(str) {
 
 module.exports.prototype = {
     // Returns a promise.
-    update_from_config:function(config) {
+    update_from_config: function(config) {
         if (config instanceof ConfigParser) {
         } else {
             return Q.reject(new Error('update_from_config requires a ConfigParser object'));
         }
+        
+        var nodearch2debarch = { 
+            'arm': 'armhf',
+            'ia32': 'i386',
+            'x64': 'amd64'
+        };
 
-        this.config = new ConfigParser(this.config_xml());
-        this.config.setName(config.name());
-        this.config.setVersion(config.version());
-        this.config.setPackageName(config.packageName());
-        this.config.setDescription(config.description());
-
-        this.config.write();
-
-        return this.update_manifest();
-    },
-
-    cordovajs_path:function(libDir) {
-        var jsPath = path.join(libDir, 'www', 'cordova.js');
-        return path.resolve(jsPath);
-    },
-
-    update_manifest: function() {
-        var nodearch2debarch = { 'arm': 'armhf',
-                                 'ia32': 'i386',
-                                 'x64': 'amd64'};
         var arch;
-        if (os.arch() in nodearch2debarch)
+        if (os.arch() in nodearch2debarch) {
             arch = nodearch2debarch[os.arch()];
-        else
-            return Q.reject(new Error('unknown cpu arch'));
-
-        if (!this.config.author())
-            return Q.reject(new Error('config.xml should contain author'));
-
-        var manifest = { name: this.config.packageName(),
-                         version: this.config.version(),
-                         title: this.config.name(),
-                         hooks: { cordova: { desktop: 'cordova.desktop',
-                                             apparmor: 'apparmor.json' } },
-                         framework: 'ubuntu-sdk-13.10',
-                         maintainer: sanitize(this.config.author())  + ' <' + this.config.doc.find('author').attrib.email + '>',
-                         architecture: arch,
-                         description: sanitize(this.config.description()) };
-
-        var name = sanitize(this.config.name()); //FIXME: escaping
-        var content = '[Desktop Entry]\nName=' + name + '\nExec=./cordova-ubuntu www/\nTerminal=false\nType=Application\nX-Ubuntu-Touch=true';
-
-        if (this.config.doc.find('icon') && this.config.doc.find('icon').attrib.src) {
-            var iconPath = path.join(this.path, 'www', this.config.doc.find('icon').attrib.src);
-            if (fs.existsSync(iconPath))
-                content += '\nIcon=www/' + this.config.doc.find('icon').attrib.src;
-            else
-                return Q.reject(new Error('icon does not exist: ' + iconPath));
         } else {
-            content += '\nIcon=qmlscene';
-            console.warn('missing icon element in config.xml');
+            return Q.reject(new Error('Unknown CPU architecture.'));
         }
-        fs.writeFileSync(path.join(this.path, 'manifest.json'), JSON.stringify(manifest));
-        fs.writeFileSync(path.join(this.path, 'cordova.desktop'), content);
 
-        var policy = { policy_groups: ['networking', 'audio'], policy_version: 1 };
+        if (!config.author()) {
+            return Q.reject(new Error('Could not find the author tag in config.xml.'));
+        }
 
-        this.config.doc.getroot().findall('./feature/param').forEach(function (element) {
-            if (element.attrib.policy_group && policy.policy_groups.indexOf(element.attrib.policy_group) === -1)
-                policy.policy_groups.push(element.attrib.policy_group);
+        var manifest = {};
+
+        // Load existing manifest if it exists to update it.
+        if (fs.existsSync(this.manifest)) {
+            manifest = JSON.parse(fs.readFileSync(this.manifest));
+        }
+
+        manifest.name = config.packageName();
+        manifest.version = config.version();
+        manifest.title = config.name();
+        manifest.author = sanitize(config.author());
+        manifest.maintainer = sanitize(config.author())  + ' <' + config.doc.find('author').attrib.email + '>';
+        manifest.description = sanitize(config.description());
+        manifest.architecture = arch;
+        manifest.framework = manifest.framework || 'ubuntu-sdk-13.10';
+        manifest.hooks = {
+            cordova: {
+                desktop: 'cordova.desktop',
+                apparmor: 'apparmor.json'
+            }
+        };
+
+        var desktopContent = [
+            '[Desktop Entry]',
+            'Name=' + sanitize(config.name()),
+            'Exec=./cordova-ubuntu www/',
+            'Terminal=false',
+            'Type=Application',
+            'X-Ubuntu-Touch=true'
+        ];
+
+        // An icon must be set for the application to run.
+        var iconNode = config.doc.find('icon');
+        var iconPath;
+        if (iconNode && iconNode.attrib.src) {
+            iconPath = path.join(this.path, iconNode.attrib.src);
+        }
+
+        if (!iconPath || !fs.existsSync(iconPath)) {
+            if (!iconPath) {
+            console.warn('Icon not defined in config.xml.');
+            } else {
+            console.warn('Cannot find icon at : ', iconPath);
+            }
+            desktopContent.push('Icon=qmlscene');
+        } else {
+            desktopContent.push('Icon=' + iconNode.attrib.src);
+        }
+
+        // Add additional policies used by features in config.xml
+        var policyGroups = ['networking', 'audio'];
+        var features = config.doc.getroot().findall('./feature/param');
+        var policyGroup;
+        features.forEach(function (feature) {
+            policyGroup = feature.attrib.policy_group;
+            if (policyGroup && policyGroups.indexOf(policyGroup) === -1) {
+                policyGroups.push(policyGroup);
+            }
         });
 
-        fs.writeFileSync(path.join(this.path, 'apparmor.json'), JSON.stringify(policy));
+        var apparmor = {
+            policy_groups: policyGroups,
+            policy_version: 1.2
+        };
+
+        // Write all the files.
+        fs.writeFileSync(this.manifest, JSON.stringify(manifest, null, 4));
+        fs.writeFileSync(this.desktop, desktopContent.join('\n'));
+        fs.writeFileSync(this.apparmor, JSON.stringify(apparmor, null, 4));
 
         return Q();
     },
 
-    config_xml:function(){
+    cordovajs_path: function(libDir) {
+        var jsPath = path.join(libDir, 'www', 'cordova.js');
+        return path.resolve(jsPath);
+    },
+
+    config_xml: function(){
         return path.join(this.path, 'config.xml');
     },
 
-    www_dir:function() {
+    www_dir: function() {
         return path.join(this.path, 'www');
     },
 
-    update_www:function() {
+    update_www: function() {
         var projectRoot = util.isCordova(this.path);
         var www = util.projectWww(projectRoot);
+        var platform_www = path.join(this.path, 'platform_www');
 
         shell.rm('-rf', this.www_dir());
-        shell.cp('-rf', www, this.path);
+        shell.mkdir(this.www_dir());
+
+        // Copy over all www assets
+        shell.cp('-rf', path.join(www, '*'), this.www_dir());
+        // Copy over all stock platform www assets (ie. cordova.js)
+        shell.cp('-rf', path.join(platform_www, '*'), this.www_dir());
     },
 
-    update_overrides:function() {
+    update_overrides: function() {
         var projectRoot = util.isCordova(this.path);
         var mergesPath = path.join(util.appDir(projectRoot), 'merges', 'ubuntu');
-        if(fs.existsSync(mergesPath)) {
+        if (fs.existsSync(mergesPath)) {
             var overrides = path.join(mergesPath, '*');
             shell.cp('-rf', overrides, this.www_dir());
         }
     },
 
     // Returns a promise.
-    update_project:function(cfg) {
+    update_project: function(cfg) {
         var self = this;
 
         return this.update_from_config(cfg)
